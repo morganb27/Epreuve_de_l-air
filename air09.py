2c64095e59ccbc34f75f43b51366c045b3e3ec27
morganb27 <115886162+morganb27@users.noreply.github.com>
2023-03-04 09:47:02 +0100

    Add files via upload

---
+++ air09.py
@@ -0,0 +1,14 @@
+import sys
+
+def ma_rotation(array):
+    i = 0
+    new_array = []
+    while i < (len(array)):
+        new_array.append(array[i])
+        i = i + 1
+    first_elem = new_array.pop(0)
+    new_array.append(first_elem)
+    return new_array
+
+resultat = ma_rotation(sys.argv[1:])
+print(", ".join(resultat))
