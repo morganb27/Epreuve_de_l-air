eec3b9ed0221852474b04531c0d93e575803876d
morganb27 <115886162+morganb27@users.noreply.github.com>
2023-03-04 08:11:12 +0100

    Add files via upload

---
+++ air07.py
@@ -0,0 +1,22 @@
+import sys
+
+def insertion_dans_un_tableau_trie(lst, new_element):
+    i = 0
+    while i < len(lst):
+        if int(new_element) < int(lst[i]):
+            i = i + 1
+        elif i == len(lst) - 1:
+            lst.append(new_element)
+            break
+        elif int(new_element) > int(lst[i]) and int(new_element) < int(lst[i + 1]):
+            lst.insert(i + 1, new_element)
+            break
+        else:
+            i = i + 1
+
+
+    return lst
+
+result = insertion_dans_un_tableau_trie(sys.argv[1:], sys.argv[-1])
+for element in result[:-1]:
+    print(element, end=" ")
